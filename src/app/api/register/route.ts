19c7fe2aa7943af505755e455525c05375b1ff10
Muhammad Hamza <102131334+Hamza65523@users.noreply.github.com>
2023-08-29 14:41:53 -0700

    ok

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -6,7 +6,7 @@ import bcrypt from "bcryptjs";
 export async function POST(req:any) {
   await connectMongoDB();
 
-  try {
+try {
     const { firstname,lastname, email, password,mobile } = await req.json();
     const user = await User.findOne({ email });
     if(user){
@@ -15,7 +15,7 @@ export async function POST(req:any) {
     const hashedPassword = await bcrypt.hash(password, 10);
     await User.create({ firstname,lastname,mobile, email, password: hashedPassword });
     return NextResponse.json({ message: "User registered." }, { status: 201 });
-  } catch (error) {
+} catch (error) {
     return NextResponse.json(
       { message: "An error occurred while registering the user." },
       { status: 500 }
